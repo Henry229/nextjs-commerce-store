4285557a0b1ef5648d3b9204c01c64bf1855faaf
henry chun <henry.chun@cseducation.com.au>
2023-10-07 16:36:25 +1100

    added mavbar-actions, button

--- components/navbar-actions.tsx
+++ components/navbar-actions.tsx
@@ -1,3 +1,32 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { ShoppingBag } from 'lucide-react';
+import Button from '@/components/ui/button';
+import { use, useEffect, useState } from 'react';
+
 export default function NavbarActions() {
-  return <div>NavbarActions</div>;
+  const [isMounted, setIsMounted] = useState(false);
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) return null;
+
+  return (
+    <div className='ml-auto flex items-center gap-x-4'>
+      <Button
+        onClick={() => router.push('/cart')}
+        className='flex items-center rounded-full bg-black px-4 py-2'
+      >
+        <ShoppingBag size={20} color='white' />
+        <span className='ml-2 text-sm font-medium text-white'>
+          2{/* {cart.items.length} */}
+        </span>
+      </Button>
+    </div>
+  );
 }
