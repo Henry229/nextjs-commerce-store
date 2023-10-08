315cbd2831111a313863a3b333217f6a9509700a
henry chun <henry.chun@cseducation.com.au>
2023-10-08 18:35:29 +1100

    done category

---
+++ hooks/use-cart.tsx
@@ -0,0 +1,12 @@
+import { Product } from '@/types';
+
+interface CartStore {
+  items: Product[];
+  addItem: (data: Product) => void;
+  removeItem: (id: string) => void;
+  removeAll: () => void;
+}
+
+export default function UseCart() {
+  return <div>UseCart</div>;
+}
